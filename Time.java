4b46679d95b57264bb24c69670dcb530057117c7
yangbaobaobao <yangbaobaobaobaobao@gmail.com>
2026-09-16 14:55:32 -0700

    Update Time.java

--- Time.java
+++ Time.java
@@ -4,6 +4,8 @@ class Time {
 	public static int hours;
 	public static int minutes;
 	public static int seconds;
+	
+	public static int currentTime = 60 * 60 * 16;
     public static void main(String[] args) {
 		hours = 14;
 		minutes = 45;
@@ -12,5 +14,7 @@ class Time {
 		double percentagePassed = ((double)secondsSinceMidnight)/(24 * 3600);
         System.out.println("Amount of seconds since midnight: " + secondsSinceMidnight);
         System.out.println("Percentage of day: " + percentagePassed * 100 + "%");
+        int timeDifference = (currentTime - secondsSinceMidnight);
+        System.out.println("Time since creation: " + (timeDifference/3600) + " hours " + ((timeDifference%3600)/60) + " minutes " + ((timeDifference%3600)%60) + " seconds ");
     }
 }
