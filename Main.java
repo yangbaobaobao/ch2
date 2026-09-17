744fbc68a7dd77838ff8f6e02f786e460dece467
yangbaobaobao <yangbaobaobaobaobao@gmail.com>
2026-09-17 14:58:11 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,18 +1,27 @@
 import java.util.*;
+import java.lang.Thread;
 class Main {
 	public static Scanner sc;
 	public static List<Person> Contenders = new ArrayList<Person>();
 	public static Person character;
+	public static void DeleteLine() {
+		System.out.print("\r\033[2k");
+        System.out.flush();
+    }
     public static void main(String[] args) {
 		sc = new Scanner(System.in);
         System.out.println("Gladiator Battles At Home:");
-        Contenders.add(new Person("Ethan", 125, 1, 1));
+        Contenders.add(new Person("Ethan", 125, 10, 10));
         Contenders.add(new Person("Ryan", 100, 10, 15));
         Contenders.add(new Person("Jiahao", 1000, 4000, 17));
         for (Person p : Contenders){
 			p.Declare();
-			p.AddAttack(new Attack("punch", p, 1.0, 30));
-			p.AddAttack(new Attack("kick", p, 0.5, 50));
+			p.AddAttack(new Attack("punch", p, 1.0, 0, 30));
+			p.AddAttack(new Attack("kick", p, 0.5, 0, 50));
+			p.AddAttack(new Attack("slap", p, 0.9, 0.5, 15));
+			if (p.name.equals("Ethan")) {
+				p.AddAttack(new Attack("kiss", p, 0.4, 1, 5));
+			}
 		}
         //Contenders.get(0).Fight(Contenders.get(1));
         boolean proceed = false;
@@ -90,23 +99,60 @@ class Attack {
 	public String name;
 	public int damage;
 	public double hitRate;
+	public double reloadRate;
 	
 	public Person parent;
 	
-	Attack(String name, Person parent, double hitRate, int damage) {
+	Attack(String name, Person parent, double hitRate, double reloadRate, int damage) {
 		this.name = name;
 		this.damage = damage;
 		this.hitRate = hitRate;
+		this.reloadRate = reloadRate;
 		this.parent = parent;
 	}
 	
+	public static void DeleteLine() {
+		System.out.print("\r\033[2k");
+        System.out.flush();
+    }
+	
 	public boolean Hit(Person enemy){
+		boolean[] bar = new boolean[10];
+		int barPos = 0;
+		bar[barPos] = true;
+		boolean gaba = true;
+		int timer = 0;
+		System.out.print("☑");
+		while (gaba) {
+			timer++;
+			try {
+				DeleteLine();
+				for (boolean b : bar) {
+					if (b){
+						System.out.print("[X]");}
+					else{
+						System.out.print("[ ]");}
+				}
+				barPos = (int)(5*Math.sin(timer) + 5);
+				System.out.print(barPos);
+				//barPos++;
+				for (int i = 0; i < bar.length; i++) {
+					bar[i] = false;}
+				bar[barPos] = true;
+				if (barPos >= 10) {barPos = 0;}
+				Thread.sleep(100);
+			}
+			catch (Exception e){}
+		}
 		if (Math.random() > hitRate){
 			System.out.println(parent.name + " tried to " + name + " " + enemy.name + ", but missed...");
 			return false;
 		}
 		else{
 			System.out.println(parent.name + " " + name + "ed " + enemy.name + "!");
+			if (Math.random() < reloadRate){
+				System.out.println(enemy.name + " is stunned!, attack again!");
+			}
 			return true;
 		}
 	}
